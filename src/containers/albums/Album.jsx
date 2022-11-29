a18fc5fb2e4918d3f9081dfbb8b7d06ed0a32e23
Nikita <OvchinnikovNE1995@yandex.ru>
2022-11-29 19:50:32 +0600

    update albums

--- src/containers/albums/Album.jsx
+++ src/containers/albums/Album.jsx
@@ -1,36 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { Carousel, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { Carousel } from "bootstrap";
 
-async function fetchAlbum(albumId) {
+async function fetchPhotos(albumId) {
   const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`);
   return await response.json();
 }
 
 export const Album = () => {
-  const params = useParams();
-  const albumId = params.id;
-  const [album, setAlbum] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const albumId = useParams().id;
+
   useEffect(() => {
-    fetchAlbum(albumId).then(setAlbum);
+    fetchPhotos(albumId).then(setPhotos);
   }, []);
 
-  const [currentPhoto, setCurrentPhoto] = useState(null);
-  const [previousPhoto, setPreviousPhoto] = useState(null);
-  const [nextPhoto, setNextPhoto] = useState(null);
-
   return (
-    <div className="album">
-      <div>
-        {album.map((album, index) => (
-          <div className="album" key={index}>
-            <img src={`${album.thumbnailUrl}`} className="img-fluid" alt="Responsive image" />
-            <div>
-              <img src={`${album.url}`} className="img-fluid" alt="Responsive image" />
-            </div>
-          </div>
+    <Container className="album">
+      <Carousel>
+        {photos.map((photo, index) => (
+          <Carousel.Item key={index}>
+            <img src={photo.url.replace("/600/", "/1300x600/")} alt={photo.title} className="photo" />
+            <Carousel.Caption>
+              <h3>{photo.title}</h3>
+              <p>{`${photo.albumId} - ${photo.id} - ${photo.thumbnailUrl}`}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
         ))}
-      </div>
-    </div>
+      </Carousel>
+    </Container>
   );
 };
--- src/containers/albums/Albums.jsx
+++ src/containers/albums/Albums.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { selectUserId } from "../../slices/users";
 
 async function fetchAlbums(userId) {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/albums`);
@@ -10,29 +12,26 @@ async function fetchAlbums(userId) {
 
 export const Albums = () => {
   const [albums, setAlbums] = useState([]);
+  const userId = useSelector(selectUserId);
+
   useEffect(() => {
-    fetchAlbums(1).then(setAlbums);
-  }, []);
+    fetchAlbums(userId).then(setAlbums);
+  }, [userId]);
 
   return (
     <Container className="albums">
       <div>
-        {albums.map((album, index) => (
-          <div className="album" key={index}>
-            <div className="card-body">
-              <p className="card-text">
-                {album.id}. {album.title}
-              </p>
-            </div>
-            <Nav activeKey="/">
-              <Nav.Item>
-                <Nav.Link as={Link} to={`/albums/${album.id}`}>
-                  Press{" "}
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </div>
-        ))}
+        {albums.length
+          ? albums.map((album) => (
+              <div className="album" key={album.id}>
+                <div className="card-body">
+                  <p className="card-text">
+                    {album.id}. {album.title} <Link to={`/albums/${album.id}`}>→</Link>
+                  </p>
+                </div>
+              </div>
+            ))
+          : "Loading..."}
       </div>
     </Container>
   );
