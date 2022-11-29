b01332265ed9aef53683c7ba74a1f6dfbafa5fb0
Nikita <OvchinnikovNE1995@yandex.ru>
2022-11-29 19:50:23 +0600

    update todos

--- src/containers/todos/index.jsx
+++ src/containers/todos/index.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { dragStart, drop, fetchTodosByUserId, selectTodos } from "../../slices/todos";
-import store from "../../store";
+import { selectUserId } from "../../slices/users";
 
 export const Todos = () => {
   const dispatch = useDispatch();
   const todos = useSelector(selectTodos);
+  const userId = useSelector(selectUserId);
 
   useEffect(() => {
-    dispatch(fetchTodosByUserId(1));
-  }, []);
+    dispatch(fetchTodosByUserId(userId));
+  }, [userId]);
 
   const renderColumn = (completed) => {
     const columnStatusClassName = completed ? "todos-column__completed" : "todos-column__not-completed";
     const columnTitle = completed ? "Completed" : "Not completed";
-    const filterCallback = (todo) => todo.completed === completed;
+    const filteredTodos = todos.filter((todo) => todo.completed === completed);
     return (
       <div
         className={`todos-column ${columnStatusClassName}`}
@@ -25,11 +26,15 @@ export const Todos = () => {
         onDragOver={(e) => e.preventDefault()}
       >
         <h2>{columnTitle}</h2>
-        {todos.filter(filterCallback).map((todo, index) => (
-          <div onDragStart={() => dispatch(dragStart(todo))} draggable={true} className="todo-card" key={index}>
-            {todo.title}
-          </div>
-        ))}
+        {filteredTodos.length ? (
+          filteredTodos.map((todo, index) => (
+            <div onDragStart={() => dispatch(dragStart(todo))} draggable={true} className="todo-card" key={index}>
+              {todo.title}
+            </div>
+          ))
+        ) : (
+          <div>No tasks</div>
+        )}
       </div>
     );
   };
